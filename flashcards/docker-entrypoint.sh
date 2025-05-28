347b9504c41ffd0b361a9282c039ef7231f1f6b0
ASETML <alban.segalen@eduvaud.ch>
2025-05-28 14:04:49 +0200

    commenter(entrypoint): Commentaire des commandes pour les ressources et le build
    [15min] [Done]

--- flashcards/docker-entrypoint.sh
+++ flashcards/docker-entrypoint.sh
@@ -13,13 +13,23 @@ node ace migration:run --force
 echo "Exécution des seeds..."
 node ace db:seed
 
-# Démarrer l'application
-echo "Démarrage de l'application..."
+# Construire l'application
+echo "Construction de l'application..."
 npm run build
+
+# Copie des fichiers css et js
+echo "Copie des ressources"
 mkdir ./build/resources/css
 cp ./resources/css/app.css ./build/resources/css/app.css
 mkdir ./build/resources/js
 cp ./resources/js/app.js ./build/resources/js/app.js
+
 cd build
+
+# Installation des dépendances
+echo "Installation des dépendances"
 npm ci --omit='dev'
+
+# Démarrer l'application
+echo "Démarrage de l'application"
 node bin/server.js
